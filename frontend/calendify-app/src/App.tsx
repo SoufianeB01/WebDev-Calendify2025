ab4cd1dba29ae8f6c24cea232bd08c8c0f33163e
kish0oe <0799542@hr.nl>
2025-11-03 08:42:07 +0100

    app.tsx oude components verwijderd

--- frontend/calendify-app/src/App.tsx
+++ frontend/calendify-app/src/App.tsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './loginPage/Login';
-import MyComponent from './MyComponent';
-import { Fun } from './Functional';
+
 
 
 function App() {
@@ -12,11 +11,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <MyComponent></MyComponent>
+    
         
         <Login></Login>
-        <Fun counter={counter} setCounter={setCounter} name={"my cuner"}></Fun>
-        <Fun counter={counter} setCounter={setCounter} name={"my cunter"}></Fun>
+
       </header>
     </div>
   );
