4cb2c43aa42ee67c96bd3bc63d12f23251279ed6
Yong <144024466+Yoyoclub@users.noreply.github.com>
2025-11-10 13:40:15 +0100

    generic template removed roomscontroller

--- backend/CalendifyWebAppAPI/Controllers/RoomsController.cs
+++ backend/CalendifyWebAppAPI/Controllers/RoomsController.cs
@@ -1,77 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using CalendifyWebAppAPI.Data;
 using CalendifyWebAppAPI.Models;
-using System.Linq;
 
 namespace CalendifyWebAppAPI.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class RoomsController : ControllerBase
-    {
-        private readonly AppDbContext _context;
-
-        public RoomsController(AppDbContext context)
-        {
-            _context = context;
-        }
-
-        [HttpPost] //post
-        public IActionResult Create([FromBody] Room room)
-        {
-            _context.Rooms.Add(room);
-            _context.SaveChanges();
-            return Ok(room);
-        }
-
-        [HttpGet] //get
-        public IActionResult GetAll()
-        {
-            var rooms = _context.Rooms.ToList();
-            return Ok(rooms);
-        }
-
-        [HttpGet("{id}")] //get
-        public IActionResult GetById(int id)
-        {
-            var room = _context.Rooms.Find(id);
-            if (room == null)
-            {
-                return NotFound(new { message = "Room not found" });
-            }
-
-            return Ok(room);
-        }
-
-        [HttpPut("{id}")] //put
-        public IActionResult Update(int id, [FromBody] Room updated)
-        {
-            var room = _context.Rooms.Find(id);
-            if (room == null)
-            {
-                return NotFound(new { message = "Room not found" });
-            }
-
-            room.RoomName = updated.RoomName;
-            room.Capacity = updated.Capacity;
-            room.Location = updated.Location;
-            _context.SaveChanges();
-            return Ok(room);
-        }
-
-        [HttpDelete("{id}")] //delete
-        public IActionResult Delete(int id)
-        {
-            var room = _context.Rooms.Find(id);
-            if (room == null)
-            {
-                return NotFound(new { message = "Room not found" });
-            }
-
-            _context.Rooms.Remove(room);
-            _context.SaveChanges();
-            return Ok(new { message = "Room deleted" });
-        }
-    }
+	[ApiController]
+	[Route("api/[controller]")]
+	public class RoomsController : GenericCrudController<Room, int>
+	{
+		public RoomsController(AppDbContext context) : base(context) { }
+	}
 }
-
